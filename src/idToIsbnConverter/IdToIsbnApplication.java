3bba4751ad278cd473c2b3faa2b9243213807a25
Avergrove <khiongyang.chua@gmail.com>
2019-02-28 22:19:30 +0800

    First Commit

---
+++ src/idToIsbnConverter/IdToIsbnApplication.java
@@ -0,0 +1,23 @@
+package idToIsbnConverter;
+
+import java.util.Scanner;
+
+/**
+ * An application that allows users to convert a product Id into an ISBN code
+ * @author Avergrove
+ */
+public class IdToIsbnApplication {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.next();
+		
+		
+		// TODO Auto-generated method stub
+		System.out.println(input);
+
+		scanner.close();
+	}
+
+}
